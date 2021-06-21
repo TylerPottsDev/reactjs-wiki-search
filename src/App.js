ab9869b1b3340e5a1a0daac8b36373468ec3b230
Tyler Potts <tylerpottsdev@gmail.com>
2021-06-21 14:41:00 +0100

    Style changes and lint fix

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
 						<div className="result" key={i}>
 							<h3>{result.title}</h3>
 							<p dangerouslySetInnerHTML={{ __html: result.snippet  }}></p>
-							<a href={url} target="_blank">Read more</a>
+							<a href={url} target="_blank" rel="noreferrer">Read more</a>
 						</div>
 					)
 				})}
